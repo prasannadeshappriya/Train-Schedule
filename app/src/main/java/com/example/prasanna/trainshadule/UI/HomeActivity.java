be2c019b6b95de9b5806db1c8e5af4e2427518ce
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-03-30 14:55:30 +0530

    Code changes and bug fixes

--- app/src/main/java/com/example/prasanna/trainshadule/UI/HomeActivity.java
+++ app/src/main/java/com/example/prasanna/trainshadule/UI/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.prasanna.trainshadule.UI;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,64 +20,76 @@ import org.ksoap2.transport.HttpTransportSE;
 
 public class HomeActivity extends AppCompatActivity {
     private Button btnTest;
+    private ProgressDialog pd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //Initialize
+        pd = new ProgressDialog(this);
         btnTest = (Button) findViewById(R.id.btnTest);
+
+        //Listeners
         btnTest.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        test test = new test();
-                        test.execute();
+                        test_method();
                     }
                 }
         );
     }
 
+    private void test_method(){
+        test test = new test(this);
+        test.execute();
+    }
 
 
     private class test extends AsyncTask<Void,Void,Void>{
-        public ProgressDialog pd;
-        public test(){
-            pd = new ProgressDialog(getApplicationContext());
-        }
-        @Override
-        protected Void doInBackground(Void... params) {
-            String methodName = "getLines";
-            SoapObject request = new SoapObject(Constants.NAMESPACE,methodName);
-            request.addProperty("lang","en");
-
-            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            envelope.setOutputSoapObject(request);
-
-            HttpTransportSE httpTransport = new HttpTransportSE(Constants.ENDPOINT);
-            String actionName = methodName;
-            try {
-                httpTransport.call(actionName, envelope);
-            }catch (Exception e){
-                Toast.makeText(getApplicationContext(),"An error occoured while getting data from the server",Toast.LENGTH_LONG).show();
-                Log.i(Constants.TAG,"Error on test AsyncTask :- " + e.toString());
-            }
-            //Need to check both of them seperately
-            Log.i(Constants.TAG,"Result :- " + envelope.bodyOut.toString());
-            Log.i(Constants.TAG,"Result :- " + envelope.bodyIn.toString());
-
-            request = (SoapObject)envelope.bodyIn;
-            String a = "Test"; //for debug request
-
-            return null;
+        private Context context;
+        public test(Context _context){
+            this.context = _context;
         }
 
         @Override
         protected void onPreExecute() {
             //super.onPreExecute();
+
             pd.setIndeterminate(true);
             pd.setCanceledOnTouchOutside(false);
             pd.setMessage("Please wait");
             pd.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String methodName = "getLines";
+                SoapObject request = new SoapObject(Constants.NAMESPACE,methodName);
+                request.addProperty("lang","en");
+
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                envelope.setOutputSoapObject(request);
+
+                HttpTransportSE httpTransport = new HttpTransportSE(Constants.ENDPOINT);
+                String actionName = methodName;
+
+                httpTransport.call(actionName, envelope);
+
+                //Need to check both of them seperately
+                Log.i(Constants.TAG,"Result :- " + envelope.bodyOut.toString());
+                Log.i(Constants.TAG,"Result :- " + envelope.bodyIn.toString());
+
+                request = (SoapObject)envelope.bodyIn;
+                String a = "Test"; //for debug request
+
+            }catch (Exception e){
+                Log.i(Constants.TAG,"Error on test AsyncTask :- " + e.toString());
+            }
+            return null;
         }
 
         @Override
@@ -84,6 +97,7 @@ public class HomeActivity extends AppCompatActivity {
             //super.onPostExecute(aVoid);
             pd.dismiss();
             Log.i(Constants.TAG,"Process test successfully executed");
+            Toast.makeText(context,"Done",Toast.LENGTH_LONG).show();
         }
     }
 }
