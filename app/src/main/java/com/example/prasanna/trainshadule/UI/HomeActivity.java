d9ace0b458a86164eedce04a9c0aa59c7f501c00
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-04-21 15:27:29 +0530

    Code Improvements Gui changes

--- app/src/main/java/com/example/prasanna/trainshadule/UI/HomeActivity.java
+++ app/src/main/java/com/example/prasanna/trainshadule/UI/HomeActivity.java
@@ -1,26 +1,18 @@
 package com.example.prasanna.trainshadule.UI;
 
 import android.app.ProgressDialog;
-import android.content.Context;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.example.prasanna.trainshadule.Constants.Constants;
 import com.example.prasanna.trainshadule.R;
-
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.ksoap2.transport.HttpTransportSE;
+import com.example.prasanna.trainshadule.ServerRequest.Request;
 
 public class HomeActivity extends AppCompatActivity {
     private Button btnTest;
     private ProgressDialog pd;
+    private Request request;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +21,7 @@ public class HomeActivity extends AppCompatActivity {
         //Initialize
         pd = new ProgressDialog(this);
         btnTest = (Button) findViewById(R.id.btnTest);
+        request = new Request(this,pd);
 
         //Listeners
         btnTest.setOnClickListener(
@@ -42,62 +35,6 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void test_method(){
-        test test = new test(this);
-        test.execute();
-    }
-
-
-    private class test extends AsyncTask<Void,Void,Void>{
-        private Context context;
-        public test(Context _context){
-            this.context = _context;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            //super.onPreExecute();
-
-            pd.setIndeterminate(true);
-            pd.setCanceledOnTouchOutside(false);
-            pd.setMessage("Please wait");
-            pd.show();
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                String methodName = "getLines";
-                SoapObject request = new SoapObject(Constants.NAMESPACE,methodName);
-                request.addProperty("lang","en");
-
-                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-                envelope.setOutputSoapObject(request);
-
-                HttpTransportSE httpTransport = new HttpTransportSE(Constants.ENDPOINT);
-                String actionName = methodName;
-
-                httpTransport.call(actionName, envelope);
-
-                //Need to check both of them seperately
-                Log.i(Constants.TAG,"Result :- " + envelope.bodyOut.toString());
-                Log.i(Constants.TAG,"Result :- " + envelope.bodyIn.toString());
-
-                request = (SoapObject)envelope.bodyIn;
-                String a = "Test"; //for debug request
-
-            }catch (Exception e){
-                Log.i(Constants.TAG,"Error on test AsyncTask :- " + e.toString());
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            //super.onPostExecute(aVoid);
-            pd.dismiss();
-            Log.i(Constants.TAG,"Process test successfully executed");
-            Toast.makeText(context,"Done",Toast.LENGTH_LONG).show();
-        }
+        request.getLines();
     }
 }
